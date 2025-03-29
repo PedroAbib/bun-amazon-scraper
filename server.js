6c05630dc6ef62e62bdd805b359cd7bcfd5a1cd0
pedro <paulo-ppaa@hotmail.com>
2025-03-29 15:32:37 -0300

    feature: implemented server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+
+// Creating Express instance and setting the server port.
+const app = express();
+const PORT = 3000;
+
+// Here I create my app endpoint that will handle the scraping request, at first I tested how to
+// access and manipulate the query parameter using query string and returning it in JSON.
+app.get('/api/scrape', (req, res) => {
+
+    // Used a try catch to help during debug, as well as preventing the application from breaking.
+    try {
+        const keyword = req.query.keyword;
+
+        res.json({keyword});
+
+    } catch (error) {
+        console.error('Error during scraping:', error);
+        res.status(500).json({ error: 'Error during scraping' });
+    }
+});
+
+// This allows me to set the server to listen to the specified port and to inform me that it's running.
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
